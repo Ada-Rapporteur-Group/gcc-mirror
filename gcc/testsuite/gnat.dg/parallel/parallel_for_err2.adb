374580b69923ecd58c8cfc1b7c4f81bc022032b0
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2026-01-07 12:36:35 -0600

    Change parallel_for8 name to parallel_for_err2

--- gcc/testsuite/gnat.dg/parallel/parallel_for_err2.adb
+++ gcc/testsuite/gnat.dg/parallel/parallel_for_err2.adb
@@ -3,7 +3,7 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 
-procedure parallel_for8 is
+procedure parallel_for_err2 is
    subtype A is Integer range 1 .. 10;
 begin
    parallel for I in 3 .. 2 loop -- { dg-error "warning: loop range is null, loop will not execute" }
@@ -14,7 +14,7 @@ begin
       for I in 1 .. 100 loop
          Put_Line ("Unreachable");
       end loop;
-end parallel_for8;
+end parallel_for_err2;
 
 -- { dg-error "warning: static value out of range of type \"A\" defined at line 7" "" { target *-*-* } 13 }
 -- { dg-error "warning: Constraint_Error will be raised at run time" "" { target *-*-* } 13 }
