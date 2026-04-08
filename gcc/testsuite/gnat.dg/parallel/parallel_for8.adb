c88fe693f7edde9a6e3958fe0d171965bcb210c9
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2026-04-08 02:18:42 -0500

    Add test

---
+++ gcc/testsuite/gnat.dg/parallel/parallel_for8.adb
@@ -0,0 +1,50 @@
+-- { dg-do run }
+-- { dg-options "-gnat2022" }
+
+with LWT.Parallelism; use LWT.Parallelism;
+
+procedure parallel_for8 is
+   type Base_Type is (A, B, C, D, E);
+   subtype Pred_Type is Base_Type
+      with Static_Predicate => Pred_Type in A | C | E;
+
+   subtype Int_Sub is Integer range 1 .. 50
+     with Static_Predicate => Int_Sub in 1 | 5 .. 25 | 47;
+
+   Visited_Enum : array (Base_Type) of Boolean := (others => False);
+   Visited_Ints : array (1 .. 50) of Boolean := (others => False);
+
+begin
+   parallel for I in Pred_Type loop
+      Visited_Enum (I) := True;
+   end loop;
+
+   parallel for I in Int_Sub loop
+      Visited_Ints (I) := True;
+   end loop;
+
+   for I in Pred_Type loop
+      if not Visited_Enum (I) then
+         raise Program_Error;
+      end if;
+   end loop;
+
+   if Visited_Enum (B) or else Visited_Enum (D) then
+      raise Program_Error;
+   end if;
+
+   for I in 1 .. 50 loop
+      if I in 1 | 5 .. 25 | 47 xor Visited_Ints (I) then
+         raise Program_Error;
+      end if;
+   end loop;
+
+   if Mock_Check_Loop (1) /= ENDED then
+      raise Program_Error;
+   end if;
+
+   if Mock_Check_Loop (2) /= ENDED then
+      raise Program_Error;
+   end if;
+
+end parallel_for8;
