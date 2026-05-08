11518f853160a995b8d40797f3e7b43d3a8f1253
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2026-05-08 10:34:26 -0500

    Add null subrange to range test

--- gcc/testsuite/gnat.dg/parallel/parallel_for_err2.adb
+++ gcc/testsuite/gnat.dg/parallel/parallel_for_err2.adb
@@ -1,21 +1,31 @@
 -- { dg-do compile }
--- { dg-options "-gnat2022 -Werror" }
-
-with Ada.Text_IO; use Ada.Text_IO;
+-- { dg-options "-gnat2022" }
 
 procedure parallel_for_err2 is
    subtype A is Integer range 1 .. 10;
+   subtype Null_Subtype is Integer range 1 .. 0;  -- { dg-error "warning: null range" }
 begin
    parallel for I in 3 .. 2 loop -- { dg-error "warning: loop range is null, loop will not execute" }
-      Put_Line ("Unreachable");
+      raise Program_Error;
    end loop;
 
-   parallel (Ch in A range 5 .. 15)
-      for I in 1 .. 100 loop
-         Put_Line ("Unreachable");
-      end loop;
+   parallel for I in Null_Subtype loop -- { dg-error "warning: loop range is null, loop will not execute" }
+      raise Program_Error;
+   end loop;
+
+   begin
+      parallel (Ch in A range 5 .. 15)
+         for I in 1 .. 100 loop
+            raise Program_Error;
+         end loop;
+   exception
+      when Constraint_Error =>
+         null;
+      when others =>
+         raise Program_Error;
+   end;
 end parallel_for_err2;
 
--- { dg-error "warning: static value out of range of type \"A\" defined at line 7" "" { target *-*-* } 13 }
--- { dg-error "warning: Constraint_Error will be raised at run time" "" { target *-*-* } 13 }
--- { dg-error "warning: Suspicious chunk_index range: out of range of chunk_index subtype. \"Constraint_Error\" will be raised at run-time." "" { target *-*-* } 13 }
+-- { dg-error "warning: static value out of range of type \"A\" defined at line 5" "" { target *-*-* } 17 }
+-- { dg-error "warning: Constraint_Error will be raised at run time" "" { target *-*-* } 17 }
+-- { dg-error "warning: Suspicious chunk_index range: out of range of chunk_index subtype. \"Constraint_Error\" will be raised at run-time." "" { target *-*-* } 17 }
