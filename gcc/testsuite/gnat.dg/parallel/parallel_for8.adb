d15521b2a57864c66163a9ca7ec0c84cf69111f0
Ethan Luis McDonough <ethanluismcdonough@gmail.com>
2026-05-08 02:20:45 -0500

    Add predicated holey subtype to test case

--- gcc/testsuite/gnat.dg/parallel/parallel_for8.adb
+++ gcc/testsuite/gnat.dg/parallel/parallel_for8.adb
@@ -11,12 +11,16 @@ procedure parallel_for8 is
    type Tens is (One, Two, Three);
    for Tens use (One => 10, Two => 20, Three => 30);
 
+   subtype Odd_Tens is Tens
+      with Static_Predicate => Odd_Tens in One | Three;
+
    subtype Int_Sub is Integer range 1 .. 50
      with Static_Predicate => Int_Sub in 1 | 5 .. 25 | 47;
 
    Visited_Enum : array (Base_Type) of Boolean := (others => False);
    Visited_Ints : array (1 .. 50) of Boolean := (others => False);
    Visited_Tens : array (Tens) of Boolean := (others => False);
+   Visited_Odds : array (Tens) of Boolean := (others => False);
 
 begin
    parallel for I in Pred_Type loop
@@ -29,6 +33,10 @@ begin
 
    parallel for I in Tens loop
       Visited_Tens (I) := True;
+   end loop;
+
+   parallel for I in Odd_Tens loop
+      Visited_Odds (I) := True;
    end loop;
 
    for I in Pred_Type loop
@@ -53,6 +61,16 @@ begin
       end if;
    end loop;
 
+   for I in Odd_Tens loop
+      if not Visited_Odds (I) then
+         raise Program_Error;
+      end if;
+   end loop;
+
+   if Visited_Odds (Two) then
+      raise Program_Error;
+   end if;
+
    if Mock_Check_Loop (1) /= ENDED then
       raise Program_Error;
    end if;
@@ -62,6 +80,10 @@ begin
    end if;
 
    if Mock_Check_Loop (3) /= ENDED then
+      raise Program_Error;
+   end if;
+
+   if Mock_Check_Loop (4) /= ENDED then
       raise Program_Error;
    end if;
 
